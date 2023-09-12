5ed15ed272a3610e3d108b7ff0064ea5bacfffed
Umer Farooq <32106483+umerfarooq786@users.noreply.github.com>
2023-09-12 17:59:21 +0500

    Update 2023_08_25_173025_create_solar_inquiries_table.php
    Constraints

--- database/migrations/2023_08_25_173025_create_solar_inquiries_table.php
+++ database/migrations/2023_08_25_173025_create_solar_inquiries_table.php
@@ -15,13 +15,13 @@ class CreateSolarInquiriesTable extends Migration
     {
         Schema::create('solar_inquiries', function (Blueprint $table) {
             $table->id();
-            $table->string('zip')->nullable;
-            $table->boolean('is_home_owner')->nullable;
-            $table->string('address')->nullable;
-            $table->string('email')->nullable;
-            $table->string('full_name')->nullable;
-            $table->string('phone')->nullable;
-            $table->text('message')->nullable;
+            $table->string('zip')->nullable();
+            $table->boolean('is_home_owner')->nullable();
+            $table->string('address')->nullable();
+            $table->string('email')->nullable();
+            $table->string('full_name')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('message')->nullable();
 
             $table->timestamps();
         });
